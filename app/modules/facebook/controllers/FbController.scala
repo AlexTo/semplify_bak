c7cdb6d7b9477dba5acb8d51f893ce840a8de0e3
Alex To <tonhudung@gmail.com>
2020-05-12 11:55:08 +1000

    wip

--- app/modules/facebook/controllers/FbController.scala
+++ app/modules/facebook/controllers/FbController.scala
@@ -14,9 +14,9 @@ class FbController @Inject()(cc: ControllerComponents)
 
   def test: Action[AnyContent] = Action.async { request =>
 
-    val appId = "3144770955585878"
-    val appSecret = "40e70bdb7ed9c5d63bf667d04d831c7f"
-    val token = "EAAAAUaZA8jlABAKHNCv94Jzz1zOmtd4HHRKXixpOuNUPsNWy60cq0VJfZCLAIK8E5hXZCDhZAapThcnLzVMWYUZAZBXLZBZBqf3ylTdCKyM8ZBxk8SZBKoXhvOrj79jIhOWImAv0djCyM56GPw5A5jxwizgPTtnq3tyxlyjxFbACqJ7KKcD29AK6jYyfDFYbZBikzsZD"
+    val appId = ""
+    val appSecret = ""
+    val token = ""
 
     val accessToken = new DefaultFacebookClient(Version.LATEST).obtainAppAccessToken(appId, appSecret)
     val fbClient = new DefaultFacebookClient(token, Version.LATEST)
